1445013d97d47edc59b2b5383582636329f5e270
BlurryMeal <sagarg2606@gmail.com>
2021-08-26 00:19:48 +0530

    updated music bot

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,74 @@
 const Discord = require('discord.js');
+const pagination = require('discord.js-pagination')
 module.exports = {
 	name: 'help',
 	description: "this is a help command!",
 	execute(message, args, cmd, client, Discord) {
-		const exampleEmbed = new Discord.MessageEmbed()
+		
+		const page1 = new Discord.MessageEmbed()
 			.setColor('#6699EE')
-			.setTitle('Commands List')
-			.setDescription('Here is the list of all the commands that will help you out on the server')
+			.setTitle('General Information')
+			.setURL('https://blurrybot.xyz')
+			.setDescription('BlurryBot is a fully fledged Music Bot developed by BlurryMeal. To get started join a voice channel and play a song by typing `b.play (Song Name)` ')
 			.setThumbnail('https://i.imgur.com/lIch4Lq.png')
 			.addFields(
-				{ name: 'b.play / b.p (song name or URL )', value: 'Plays that song in the voice channel you are in.' },
-				{ name: 'b.skip / b.s', value: 'Skips the song.' },
-				{ name: 'b.leave / b.stop', value: 'Leaves the VC it is in.' },
-
+				{ name: 'Commands', value: 'A Full List of commands have been mentioned in the next two pages.' },
+				{ name: 'No Sir', value: 'There is no premium section for this bot, all features including filters are free for all users!' },
+				{ name: 'Bot Invite', value: 'You can invite BlurryBot to your discord server through https://blurrybot.xyz' },
+				{ name: 'Website', value: 'For more information you can visit our website at https://blurrybot.xyz' },
 			)
-			.setTimestamp()
-			.setFooter('BlurryBot');
+		
+		const page2 = new Discord.MessageEmbed()
+			.setColor('#6699EE')
+			.setTitle('General Music Commands')
+			.setURL('https://blurrybot.xyz')
+			.setDescription('Here is the list of all the commands currently present in BlurryBot.')
+			.setThumbnail('https://i.imgur.com/lIch4Lq.png')
+			.addFields(
+				{ name: 'b.help', value: 'Displays this message.' },
+				{ name: 'b.play (Song Name / URL)', value: 'Plays the desired song in your voice channel.' },
+				{ name: 'b.skip', value: 'Skips the current song.' },
+				{ name: 'b.stop', value: 'Leaves the voice channel.' },
+				{ name: 'b.pause', value: 'Pauses the current song.' },
+				{ name: 'b.resume', value: 'Continues playing the song.' },
+				{ name: 'b.loop | b.repeat', value: 'Loops the song currently playing in the voice channel.' },
+				{ name: 'b.volume (Numerical Value)', value: 'Changes the volume of the song.' },
+				{ name: 'b.jump (Number)', value: 'Jumps to the preferred song in the queue.' },
+				{ name: 'b.shuffle', value: 'Shuffles the Queue.' },
+			)
 
-		message.channel.send(exampleEmbed);
+		const page3 = new Discord.MessageEmbed()
+			.setColor('#6699EE')
+			.setTitle('Music Filter Commands')
+			.setURL('https://blurrybot.xyz')
+			.setDescription('Here is the list of all the commands for Music Filters')
+			.setThumbnail('https://i.imgur.com/lIch4Lq.png')
+			.addFields(
+				{ name: 'b.3d', value: 'This Filter gives a 3D audio effect for the current song.' },
+				{ name: 'b.echo', value: 'This Filter gives an echo effect for the current song.' },
+				{ name: 'b.karaoke', value: 'Always wanted to sing on discord? Well now is your chance!' },
+				{ name: 'b.reverse', value: 'This Filter reverses the audio of the current song.' },
+				{ name: 'b.surround', value: 'This Filter gives a Surround Sound effect for the current song.' },
+				{ name: 'b.earwax', value: 'Never had earwax in your ears?' },
+				{ name: 'b.bassboost', value: 'Increase the Bass of the current song.' },
+				{ name: 'b.nightcore', value: 'This FIlter increases the pitch and pace of the current song.' },
+				{ name: 'b.vaporwave', value: 'This Filter makes the current song sound like as if it was made in the 1980s.' },
+			)
+	
+			
+		const pages = [
+			page1,
+			page2,
+			page3
+		]
+
+		const emoji = ["⏪", "⏩"]
+
+		const timeout = '30000'
+
+		pagination(message, pages, emoji, timeout)
+	
+
 	}
+
 }
--- index.js
+++ index.js
@@ -1,6 +1,162 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const DisTube = require('distube'),
+config = {
+	prefix: "b.",
+};
+
+// Create a new DisTube
+const distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true });
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (message) => {
+if (message.author.bot) return;
+if (!message.content.startsWith(config.prefix)) return;
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift();
+
+if (command == "play")
+	distube.play(message, args.join(" "));
+
+if (["repeat", "loop"].includes(command))
+	distube.setRepeatMode(message, parseInt(args[0]));
+	
+if (command == "loop" || command == "repeat") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Current Song has been Looped!')
+
+	message.channel.send(exampleEmbed);
+}
+
+if (command == "stop") {
+	distube.stop(message);
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Bot has left the voice channel!')
+
+	message.channel.send(exampleEmbed);
+}
+if (command == "skip") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Song has been skipped!')
+
+	message.channel.send(exampleEmbed);
+}
+
+if (command == "skip")
+	distube.skip(message);
+
+if (command == "volume")
+distube.setVolume(message, args[0]);
+if (command == "volume") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle(`Volume has been set to ${args[0]}% `)
+
+	message.channel.send(exampleEmbed);
+}
+
+if (command == "jump")
+distube.jump(message, parseInt(args[0]-1));
+
+if (command == "shuffle")
+distube.shuffle(message);
+
+if (command == "shuffle") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle(`Queue has been shuffled!`)
+
+	message.channel.send(exampleEmbed);
+}
+
+if (command == "pause")
+distube.pause(message);
+
+
+if (command == "pause") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle(`Current Song has been paused!`)
+
+	message.channel.send(exampleEmbed);
+}
+
+
+if (command == "resume")
+distube.resume(message);
+
+if (command == "resume") {
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle(`Current Song has been resumed!`)
+
+	message.channel.send(exampleEmbed);
+}
+
+if (command == "queue") {
+	let queue = distube.getQueue(message);
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Current Queue')
+	.setDescription(queue.songs.map((song,id) =>
+	`**${id + 1}**) ${song.name} - \`${song.formattedDuration}\``).slice(0,10).join("\n"))
+
+	message.channel.send(exampleEmbed);
+}
+
+if ([`3d`, `bassboost`, `echo`, `karaoke`, `nightcore`, `vaporwave`, `reverse`, `surround`, `earwax`].includes(command)) {
+	let filter = distube.setFilter(message, command);
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Current Filter')
+	.setDescription(filter || 'Off')
+
+	message.channel.send(exampleEmbed);
+}
+});
+
+// Queue status template
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+// DisTube event listeners, more in the documentation page
+distube
+
+.on("playSong", (message, queue, song) => 
+message.channel.send(
+	`Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+))
+.on("addSong", (message, queue, song) => message.channel.send(
+	`Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+))
+.on("playList", (message, queue, playlist, song) => message.channel.send(
+	`Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+))
+.on("addList", (message, queue, playlist) => message.channel.send(
+	`Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+))
+// DisTubeOptions.searchSongs = true
+.on("searchResult", (message, result) => {
+	let i = 0;
+	const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#9966CC')
+	.setTitle('Choose an option from below')
+	.setDescription(`${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n\n*Enter anything else or wait 60 seconds to cancel*`)
+	message.channel.send(exampleEmbed);
+	
+})
+// DisTubeOptions.searchSongs = true
+.on("searchCancel", (message) => message.channel.send(`Searching canceled`))
+.on("error", (message, e) => {
+	console.error(e)
+	message.channel.send("An error encountered: " + e);
+});
 
 
 client.commands = new Discord.Collection();
